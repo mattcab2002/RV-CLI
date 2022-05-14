34f3f5cd7a42a0733431282883c096115f403bbc
Matthew Cabral <matthew.larbac@gmail.com>
2022-05-14 18:36:20 -0400

    Added CL select feature if flags missing, /components default target directory

--- src/cli.js
+++ src/cli.js
@@ -1,50 +1,114 @@
 import arg from 'arg';
 import inquirer from 'inquirer';
+import { rv } from './main';
 
 function parseArgumentsIntoOptions(rawArgs) {
- const args = arg(
-   {
-        '--class': Boolean,
-        '--eject': Boolean,
-        '-c':'--class',
-        '-e':'--eject'
-   },
-   {
-        argv: rawArgs.slice(2),
-   }
- );
- return {
-        type: args['--class'] ? 'class' : 'functional',
-        eject: args['--eject'] || false,
-        command: args._[0].split(':')[0],
-        object: args._[0].split(':')[1],
-        framework: args._[1],
- };
-}
+	const args = arg(
+		{
+			'--react': Boolean,
+			'--vue':Boolean,
+			'--javascript': Boolean,
+			'--typescript': Boolean,
+			'--class': Boolean,
+			'--functional': Boolean,
+			'--eject': Boolean,
+			'-r':'--react',
+			'-v':'--vue',
+			'-js':'--javascript',
+			'-ts':'--typescript',
+			'-c':'--class',
+			'-f':'--functional',
+			'-e':'--eject'
+		},
+		{
+			argv: rawArgs.slice(2),
+		}
+		);
+		return {
+			eject: args['--eject'] || false,
+			command: args._[0].split(':')[0],
+			object: args._[0].split(':')[1],
+			name: args._[1],
+			framework: args['--react'] ? 'React' : false || args['--vue'] ? 'Vue' : false || false,
+			template: args['--javascript'] ? 'Javascript' : false || args['--typescript'] ? 'Typescript' : false,
+			type: args['--class'] ? 'Class' : false || args['--functional'] ? 'Functional' : false || false
+		};
+	}
+	
+	async function promptForMissingOptions(options) {
+		const framework = 'React';
+		const template = 'Javascript';
+		const type = 'Functional';
+		const questions = [];
+		
+		if (!options.framework) {
+			questions.push({
+				type: 'list',
+				name: 'framework',
+				message: 'Please choose which project framework to use',
+				choices: ['React', 'Vue'],
+				default: framework,
+			});
+			
+			questions.push({	
+				type: 'list',
+				name: 'template',
+				message: 'Please choose which project template to use',
+				choices: ['Javascript', 'Typescript'],
+				default: template,	
+				when: (answers) => answers.framework === 'React'
+			});
+			
+			questions.push({		
+				type: 'list',
+				name: 'type',
+				message: 'Please choose which project type of file to use',
+				choices: ['Class','Functional'],
+				default: type,
+				when: (answers) => answers.framework === 'React' && typeof answers.template === 'string'
+			});
+		}
 
-async function promptForMissingOptions(options) {
- const defaultTemplate = 'JavaScript';
- const questions = [];
-
- if (!options.framework) {
-   questions.push({
-     type: 'list',
-     name: 'framework',
-     message: 'Please choose which project framework to use',
-     choices: ['React', 'Vue'],
-     default: defaultTemplate,
-   });
- }
-const answers = await inquirer.prompt(questions);
-return {
-   ...options,
-   framework: options.framework || answers.framework,
- };
-}
-
-export async function cli(args) {
-        let options = parseArgumentsIntoOptions(args);
-        options = await promptForMissingOptions(options);
-        console.log(options);
-}
-
+		if(options.framework === 'React' && !options.template){
+			questions.push({
+				type: 'list',
+				name: 'template',
+				message: 'Please choose which project template to use',
+				choices: ['Javascript', 'Typescript'],
+				default: template,
+			});
+			questions.push({		
+				type: 'list',
+				name: 'type',
+				message: 'Please choose which project type of file to use',
+				choices: ['Class','Functional'],
+				default: type,
+				when: (answers) => typeof answers.template === 'string'
+			});
+		}
+		
+		if(options.framework === 'React' && !options.type){
+			questions.push({
+				type:'list',
+				name:'type',
+				message:'Please choose which project type of file to use',
+				choices: ['Class','Functional'],
+				default: type,
+			})
+		}
+		
+		const answers = await inquirer.prompt(questions);
+		return {
+			...options,
+			framework: answers.framework || options.framework,
+			template: answers.template || options.template,
+			type: answers.type || options.type,
+		};
+	}
+	
+	export async function cli(args) {
+		let options = parseArgumentsIntoOptions(args);
+		options = await promptForMissingOptions(options);
+		await rv(options);
+	}
+	
---
+++ src/main.js
@@ -0,0 +1,55 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import ncp from 'ncp';
+import path from 'path';
+import { promisify } from 'util';
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles(options) {
+  return copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+export async function rv(options) {
+  options = {
+    ...options,
+    targetDirectory: options.targetDirectory || path.join(process.cwd(),'/components'),
+  };
+  
+  const currentFileUrl = import.meta.url;
+  let templateDir = "";
+  if(options.framework.toLowerCase() === "react"){
+    templateDir = path.resolve(
+      new URL(currentFileUrl).pathname,
+      '../../templates',
+      options.framework.toLowerCase(),
+      options.template.toLowerCase(),
+      options.type.toLowerCase()
+      );
+      console.log(templateDir);
+    } else {
+    templateDir = path.resolve(
+      new URL(currentFileUrl).pathname,    
+      '../../templates',    
+      options.framework.toLowerCase()
+      )
+    }
+      options.templateDirectory = templateDir;
+
+    try {
+      await access(templateDir, fs.constants.R_OK);
+    } catch (err) {
+      console.error('%s Invalid template name', chalk.red.bold('ERROR'));
+      process.exit(1);
+    }
+    
+    console.log('Copy project files');
+    await copyTemplateFiles(options);
+    
+    console.log('%s Project ready', chalk.green.bold('DONE'));
+    return true;
+  }
+  
